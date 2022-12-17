9a0c890fd258b78bf112697ee9717b0909ace4e0
Cycor <6014101+Cycor@users.noreply.github.com>
2022-12-17 23:51:36 +0100

    Update run.sh

--- smtp_to_telegram/run.sh
+++ smtp_to_telegram/run.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bashio
-set -e
+#!/usr/bin/with-contenv bashio
 
 CONFIG_PATH=/data/options.json
 
 export ST_TELEGRAM_BOT_TOKEN="$(bashio::config 'ST_TELEGRAM_BOT_TOKEN')"
 export ST_TELEGRAM_CHAT_IDS="$(bashio::config 'ST_TELEGRAM_CHAT_IDS')"
+
+echo "Starting up!"
 
 /smtp_to_telegram
